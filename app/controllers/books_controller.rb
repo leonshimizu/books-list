10d6c17394c0d40b14b128cd188cf9284226d0e0
“leonshimizu” <lmshimizu@gmail.com>
2021-10-18 11:24:36 -0700

    books create

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -3,4 +3,14 @@ class BooksController < ApplicationController
     books = Book.all
     render json: books.as_json
   end
+
+  def create
+    book = Book.new(
+      title: params[:title],
+      author: params[:author],
+      year: params[:year]
+    )
+    book.save
+    render json: book.as_json
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  get "books" => "books#index"
+  get "/books" => "books#index"
+  post "/books" => "books#create"
 end
